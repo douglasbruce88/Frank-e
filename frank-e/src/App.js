4540e47e3a48f9ab072d69c422bb9dd9c171c7f8
Doug Bruce <douglasbruce88@gmail.comn>
2017-06-01 16:28:58 +0100

    Messing around & learning Part I

--- frank-e/src/App.js
+++ frank-e/src/App.js
@@ -2,17 +2,55 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var ipsumText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.';
+
+var buttonStyle = {
+  margin: '10px 10px 10px 0'
+};
+
+// There are multiple ways to implement this component. This is one.
+var VacancySign = React.createClass({
+  render: function() {
+    var text;
+    if (this.props.hasvacancy) {
+      text = 'Vacancy';
+    } else {
+      text = 'No Vacancy';
+    }
+    return <div>{text}</div>;
+  }
+});
+
+var BannerAd = React.createClass({
+  onMagicClick: function(evt) {
+    alert('TAADAH!')
+  },
+
+  render: function() {
+    // Render the div with an onClick prop (value is a function)
+    return <button onClick={this.onMagicClick}>Do Magic</button>
+  }
+});
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Say hello to Frank-e</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <button style = {buttonStyle}>Hello</button>
+          <div>{ipsumText}</div>
+        </div>
+        <br/>
+        <VacancySign hasvacancy={true} />
+        <br/>
+        <BannerAd/>
       </div>
     );
   }
